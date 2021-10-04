8aed5786a7d9e720ab9dab675e0a4ee80e09e2c1
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-10-04 16:15:50 +0200

    Quienes somos: Añadiendo textos sin estilos

---
+++ components/Aboutus.js
@@ -0,0 +1,50 @@
+import Image from "next/image";
+
+function Aboutus() {
+  return (
+    <div>
+      <h1>¿Qué es GDG?</h1>
+      <div>
+        Los Google Developer Groups (GDGs) son entidades sin ánimo de lucro
+        creadas con el objetivo de mejorar la comunidad de entusiastas
+        tecnológicos y aumentar el conocimiento y las relaciones entre las
+        personas que se dedican a esta área.
+      </div>
+      <h2>¿Qué hacemos?</h2>
+      <div>
+        Albergamos una variedad de actividades técnicas para desarrolladores,
+        desde solo unas pocas personas que se reúnen para ver nuestros últimos
+        videos, grandes reuniones con demostraciones y charlas tecnológicas, o
+        hackathons.
+      </div>
+      <div>
+        <div>
+          <h2>Conecta</h2>
+          <p>
+            Todos son bienvenidos, ya sea que vengas de una profesión distinta o
+            te pases a visitar.
+          </p>
+        </div>
+        <div>
+          <h2>Aprende</h2>
+          <p>
+            Aprenda sobre una variedad de temas técnicos y obtenga nuevas
+            habilidades a través de talleres prácticos, capacitación, eventos,
+            charlas y reuniones, tanto en línea como en persona.
+          </p>
+        </div>
+        <div>
+          <h2>Crece</h2>
+          <p>
+            Aplique sus conocimientos y conexiones para crear excelentes
+            productos y avanzar en sus habilidades, carrera y red. Da de vuelta
+            a tu comunidad ayudando a otros a aprender también.
+          </p>
+        </div>
+      </div>
+      <div>Gráfico</div>
+    </div>
+  );
+}
+
+export default Aboutus;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Image from "next/image";
 import heroImage from "../public/heroImage.png";
+import AboutUs from "../components/Aboutus";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <div className="w-full">
           <Image src={heroImage} />
         </div>
+        <AboutUs />
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
