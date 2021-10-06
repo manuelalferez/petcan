46fe104d98c1886670882a44cb51de575aa5ff50
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-10-06 19:01:12 +0200

    Añadiendo equipo

---
+++ components/Team.js
@@ -0,0 +1,94 @@
+import Image from "next/image";
+import data from "../data/TeamData";
+
+function Team() {
+  let list = [];
+  for (let organizer of data) {
+    list.push(
+      <div className="p-2 w-1/2 md:w-1/3 lg:w-1/4 flex flex-col items-center pb-6">
+        <img src={organizer.picture} className="h-24 w-24 rounded-full" />
+        <div>{organizer.name}</div>
+        <div className="flex justify-center">
+          {(() => {
+            let socialItems = [];
+            if (organizer.social.hasOwnProperty("github")) {
+              socialItems.push(
+                <a
+                  className="p-2 hover:bg-gray-100 rounded-full"
+                  href={organizer.social["github"]}
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/733/733609.png"
+                    className="h-6"
+                  />
+                </a>
+              );
+            }
+            if (organizer.social.hasOwnProperty("website")) {
+              socialItems.push(
+                <a
+                  className="p-2 hover:bg-gray-100 rounded-full"
+                  href={organizer.social["website"]}
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/1231/1231223.png"
+                    className="h-6"
+                  />
+                </a>
+              );
+            }
+            if (organizer.social.hasOwnProperty("linkedin")) {
+              socialItems.push(
+                <a
+                  className="p-2 hover:bg-gray-100 rounded-full"
+                  href={organizer.social["linkedin"]}
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/733/733617.png"
+                    className="h-6"
+                  />
+                </a>
+              );
+            }
+            if (organizer.social.hasOwnProperty("twitter")) {
+              socialItems.push(
+                <a
+                  className="p-2 hover:bg-gray-100 rounded-full"
+                  href={organizer.social["twitter"]}
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/733/733635.png"
+                    className="h-6 "
+                  />
+                </a>
+              );
+            }
+            if (organizer.social.hasOwnProperty("instagram")) {
+              socialItems.push(
+                <a
+                  className="p-2 hover:bg-gray-100 rounded-full"
+                  href={organizer.social["instagram"]}
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/733/733614.png"
+                    className="h-6"
+                  />
+                </a>
+              );
+            }
+
+            return socialItems;
+          })()}
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="p-4 md:p-6">
+      <h1 className="text-blue-500 text-3xl">Equipo</h1>
+      <div className="pt-4 flex flex-wrap">{list}</div>
+    </div>
+  );
+}
+
+export default Team;
---
+++ data/TeamData.js
@@ -0,0 +1,64 @@
+export default [
+  {
+    name: "Rafael Gómez",
+    role: "Organizador",
+    picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+    social: {
+      github: "https://github.com/rgomez96",
+    },
+  },
+  {
+    name: "Abdallah Abedraba",
+    role: "Organizador",
+    picture: "https://avatars.githubusercontent.com/u/27779735?v=4",
+    social: {
+      github: "https://github.com/aabedraba",
+      website: "https://www.aabedraba.com/",
+    },
+  },
+  {
+    name: "Manuel Alférez",
+    role: "Organizador",
+    picture: "https://avatars.githubusercontent.com/u/38152841?v=4",
+    social: {
+      github: "https://github.com/manuelalferez",
+      website: "https://www.manuelalferez.com/",
+    },
+  },
+  {
+    name: "José Honrubia",
+    role: "Organizador",
+    picture:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQHD8Q7rY6gE1g/profile-displayphoto-shrink_800_800/0/1594379775090?e=1639008000&v=beta&t=Ozb942mT8i1Au3a8ju2glxDlTkkvLdwIdR29XPX7XuM",
+    social: {
+      linkedin: "https://www.linkedin.com/in/jhonrubia/",
+    },
+  },
+  {
+    name: "Jose Manuel Rus",
+    role: "Organizador",
+    picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+    social: {
+      linkedin:
+        "https://www.linkedin.com/in/jose-manuel-rus-manj%C3%B3n-72682116b",
+    },
+  },
+  {
+    name: "Mara Luque",
+    role: "Organizador",
+    picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+    social: {
+      twitter: "https://twitter.com/m4r4__",
+      instagram: "https://www.instagram.com/maraluquee",
+    },
+  },
+  {
+    name: "Jesús Enrique",
+    role: "Organizador",
+    picture: "https://avatars.githubusercontent.com/u/50735312?v=4",
+    social: {
+      github: "https://github.com/jesi-rgb",
+      website: "https://thinking-thoughts.vercel.app/",
+    },
+  },
+];
