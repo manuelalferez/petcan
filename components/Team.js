e3cf4279735792a6fcbb9f3c0d80281673b2a387
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-10-07 13:17:12 +0200

    Extrayendo datos y simplificando en team

--- components/Team.js
+++ components/Team.js
@@ -2,7 +2,7 @@ import data from "../data/teamData";
 
 function Team() {
   let list = [];
-  for (let organizer of data) {
+  for (let organizer of data.teamMembers) {
     list.push(
       <div className="p-2 w-1/2 md:w-1/3 lg:w-1/4 flex flex-col items-center pb-6">
         <img src={organizer.picture} className="h-24 w-24 rounded-full" />
@@ -10,77 +10,19 @@ function Team() {
         <div className="flex justify-center">
           {(() => {
             let socialItems = [];
-            if (organizer.social.hasOwnProperty("github")) {
-              socialItems.push(
-                <a
-                  className="p-2 hover:bg-gray-100 rounded-full"
-                  target="_blank"
-                  href={organizer.social["github"]}
-                >
-                  <img
-                    src="https://ik.imagekit.io/gdgjaen/icons/github_AtbbS2Z3p.svg?updatedAt=1633596138135"
-                    className="h-6"
-                  />
-                </a>
-              );
-            }
-            if (organizer.social.hasOwnProperty("website")) {
-              socialItems.push(
-                <a
-                  className="p-2 hover:bg-gray-100 rounded-full"
-                  target="_blank"
-                  href={organizer.social["website"]}
-                >
-                  <img
-                    src="https://ik.imagekit.io/gdgjaen/icons/website_U32SRE1c3.svg?updatedAt=1633596138786"
-                    className="h-6"
-                  />
-                </a>
-              );
-            }
-            if (organizer.social.hasOwnProperty("linkedin")) {
-              socialItems.push(
-                <a
-                  className="p-2 hover:bg-gray-100 rounded-full"
-                  target="_blank"
-                  href={organizer.social["linkedin"]}
-                >
-                  <img
-                    src="https://ik.imagekit.io/gdgjaen/icons/linkedin_e_u8VA8N1.svg?updatedAt=1633596886592"
-                    className="h-6"
-                  />
-                </a>
-              );
-            }
-            if (organizer.social.hasOwnProperty("twitter")) {
-              socialItems.push(
-                <a
-                  className="p-2 hover:bg-gray-100 rounded-full"
-                  target="_blank"
-                  href={organizer.social["twitter"]}
-                >
-                  <img
-                    src="https://ik.imagekit.io/gdgjaen/icons/twitter_zwAdR2wfA.svg?updatedAt=1633596138541"
-                    className="h-6 "
-                  />
-                </a>
-              );
-            }
-            if (organizer.social.hasOwnProperty("instagram")) {
-              socialItems.push(
-                <a
-                  className="p-2 hover:bg-gray-100 rounded-full"
-                  target="_blank"
-                  href={organizer.social["instagram"]}
-                >
-                  <img
-                    src="https://ik.imagekit.io/gdgjaen/icons/instagram_iKyx6jfP0.svg?updatedAt=1633596138350"
-                    className="h-6"
-                  />
-                </a>
-              );
-            }
-
+            data.socialList.map((socialItem) => {
+              if (organizer.social.hasOwnProperty(socialItem)) {
+                socialItems.push(
+                  <a
+                    className="p-2 hover:bg-gray-100 rounded-full"
+                    target="_blank"
+                    href={organizer.social["github"]}
+                  >
+                    <img src={data.socialIcons[socialItem]} className="h-6" />
+                  </a>
+                );
+              }
+            });
             return socialItems;
           })()}
         </div>
--- data/TeamData.js
+++ data/TeamData.js
@@ -1,67 +1,92 @@
-export default [
-  {
-    name: "Rafael Gómez",
-    role: "Organizador",
-    picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
-    social: {
-      github: "https://github.com/rgomez96",
+export default {
+  teamMembers: [
+    {
+      name: "Rafael Gómez",
+      role: "Organizador",
+      picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+      social: {
+        github: "https://github.com/rgomez96",
+      },
     },
-  },
-  {
-    name: "Abdallah Abedraba",
-    role: "Organizador",
-    picture: "https://avatars.githubusercontent.com/u/27779735?v=4",
-    social: {
-      github: "https://github.com/aabedraba",
-      website: "https://www.aabedraba.com/",
-      twitter: "https://twitter.com/aabedraba",
+    {
+      name: "Abdallah Abedraba",
+      role: "Organizador",
+      picture: "https://avatars.githubusercontent.com/u/27779735?v=4",
+      social: {
+        github: "https://github.com/aabedraba",
+        website: "https://www.aabedraba.com/",
+        twitter: "https://twitter.com/aabedraba",
+      },
     },
-  },
-  {
-    name: "Manuel Alférez",
-    role: "Organizador",
-    picture: "https://avatars.githubusercontent.com/u/38152841?v=4",
-    social: {
-      github: "https://github.com/manuelalferez",
-      website: "https://www.manuelalferez.com/",
-      twitter: "https://twitter.com/manuelalferez",
+    {
+      name: "Manuel Alférez",
+      role: "Organizador",
+      picture: "https://avatars.githubusercontent.com/u/38152841?v=4",
+      social: {
+        github: "https://github.com/manuelalferez",
+        website: "https://www.manuelalferez.com/",
+        twitter: "https://twitter.com/manuelalferez",
+      },
     },
-  },
-  {
-    name: "José Honrubia",
-    role: "Organizador",
-    picture:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQHD8Q7rY6gE1g/profile-displayphoto-shrink_800_800/0/1594379775090?e=1639008000&v=beta&t=Ozb942mT8i1Au3a8ju2glxDlTkkvLdwIdR29XPX7XuM",
-    social: {
-      linkedin: "https://www.linkedin.com/in/jhonrubia/",
+    {
+      name: "José Honrubia",
+      role: "Organizador",
+      picture:
+        "https://media-exp1.licdn.com/dms/image/C4D03AQHD8Q7rY6gE1g/profile-displayphoto-shrink_800_800/0/1594379775090?e=1639008000&v=beta&t=Ozb942mT8i1Au3a8ju2glxDlTkkvLdwIdR29XPX7XuM",
+      social: {
+        linkedin: "https://www.linkedin.com/in/jhonrubia/",
+      },
     },
-  },
-  {
-    name: "Jose Manuel Rus",
-    role: "Organizador",
-    picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
-    social: {
+    {
+      name: "Jose Manuel Rus",
+      role: "Organizador",
+      picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+      social: {
+        linkedin:
+          "https://www.linkedin.com/in/jose-manuel-rus-manj%C3%B3n-72682116b",
+      },
+    },
+    {
+      name: "Mara Luque",
+      role: "Organizador",
+      picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+      social: {
+        twitter: "https://twitter.com/m4r4__",
+        instagram: "https://www.instagram.com/maraluquee",
+      },
+    },
+    {
+      name: "Jesús Enrique",
+      role: "Organizador",
+      picture: "https://avatars.githubusercontent.com/u/50735312?v=4",
+      social: {
+        github: "https://github.com/jesi-rgb",
+        website: "https://thinking-thoughts.vercel.app/",
+        twitter: "https://twitter.com/jesi_rgb",
+      },
+    },
+  ],
+  socialList: ["github", "website", "twitter", "linkedin", "instagram"],
+  socialIcons: [
+    {
+      github:
+        "https://ik.imagekit.io/gdgjaen/icons/github_AtbbS2Z3p.svg?updatedAt=1633596138135",
+    },
+    {
+      website:
+        "https://ik.imagekit.io/gdgjaen/icons/website_U32SRE1c3.svg?updatedAt=1633596138786",
+    },
+    {
+      twitter:
+        "https://ik.imagekit.io/gdgjaen/icons/twitter_zwAdR2wfA.svg?updatedAt=1633596138541",
+    },
+    {
       linkedin:
-        "https://www.linkedin.com/in/jose-manuel-rus-manj%C3%B3n-72682116b",
+        "https://ik.imagekit.io/gdgjaen/icons/linkedin_e_u8VA8N1.svg?updatedAt=1633596886592",
     },
-  },
-  {
-    name: "Mara Luque",
-    role: "Organizador",
-    picture: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
-    social: {
-      twitter: "https://twitter.com/m4r4__",
-      instagram: "https://www.instagram.com/maraluquee",
+    {
+      instagram:
+        "https://ik.imagekit.io/gdgjaen/icons/github_AtbbS2Z3p.svg?updatedAt=1633596138135",
     },
-  },
-  {
-    name: "Jesús Enrique",
-    role: "Organizador",
-    picture: "https://avatars.githubusercontent.com/u/50735312?v=4",
-    social: {
-      github: "https://github.com/jesi-rgb",
-      website: "https://thinking-thoughts.vercel.app/",
-      twitter: "https://twitter.com/jesi_rgb",
-    },
-  },
-];
+  ],
+};
